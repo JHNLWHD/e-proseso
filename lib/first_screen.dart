d99282019c8a2f6e0e69b74e51b80b3a701ddfb2
JHNLWHD <aljhoenilw@gmail.com>
2019-10-11 21:48:57 +0800

    :construction: initial design for home

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(color: Colors.blue[100]),
+    return MaterialApp(
+      title: 'E-Proseso',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('E-Proseso'),
+        ),
+        body: Center(
+          //child: RandomWords(),
+        ),
+      ),
     );
   }
 }
